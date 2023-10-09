3dac059f92d523185206e005db427c3601de7267
prtpj1 <prtpj1@gmail.com>
2023-10-09 20:26:34 -0300

    :memo: Starting the project video-titler-ai-web

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+
+import { Button } from './components/ui/button'
+
+function App() {
+  
+  return (
+    <>
+      <h1>Hello World!</h1>
+      <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
